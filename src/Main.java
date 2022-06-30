129e75f62447788447b2625e7d7ee3a97b2ac8b2
Azhaan Ali <azhaanali@live.com>
2022-06-29 22:09:27 -0700

    Added the ability for player to choose game-rule on start.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
-
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main (String[] args) {
 
-        AI ai = new AI();
+        Scanner s = new Scanner(System.in);
+        System.out.print("Do you want to play with overflow enabled? (y/n): ");
+        boolean overflow = s.nextLine().toUpperCase().charAt(0) == 'Y';
+
+        System.out.println("\n");
+
+        AI ai = new AI(overflow);
         ai.gameLoop();
 
     }
